63f4c6f7f566de7392647cc15a32f125acb6f673
Andreas Peters <ap@aventer.biz>
2022-12-19 11:49:59 +0100

    First in

---
+++ controller.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// reconcileReplicaSet reconciles ReplicaSets
+type reconcileReplicaSet struct {
+	// client can be used to retrieve objects from the APIServer.
+	client client.Client
+}
+
+// Implement reconcile.Reconciler so the controller can reconcile objects
+var _ reconcile.Reconciler = &reconcileReplicaSet{}
+
+func (r *reconcileReplicaSet) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	// set up a convenient log object so we don't have to type request over and over again
+	log := log.FromContext(ctx)
+
+	// Fetch the pod from the cache
+	pod := &corev1.Pod{}
+	err := r.client.Get(ctx, request.NamespacedName, pod)
+	if errors.IsNotFound(err) {
+		log.Error(nil, "Could not find Pods")
+		return reconcile.Result{}, nil
+	}
+
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("could not fetch pods: %+v", err)
+	}
+
+	// if the Namespace is a system one, do nothing.
+	if request.Namespace == "kube-system" || strings.Contains(request.Namespace, "vmware") || strings.Contains(request.Namespace, "kubernetes") {
+		return reconcile.Result{}, nil
+	}
+
+	registry := strings.Split(pod.Spec.Containers[0].Image, "/")
+
+	if registry[0] == "avhost" {
+		log.Info("Found docker.io")
+		pod.Spec.Containers[0].Image = "otherrepo" + pod.Spec.Containers[0].Image
+	}
+
+	// Print the ReplicaSet
+	log.Info("Reconciling Pods", "container name", pod.Spec.Containers[0].Image)
+
+	err = r.client.Update(ctx, pod)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("could not write ReplicaSet: %+v", err)
+	}
+
+	return reconcile.Result{}, nil
+}
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+func init() {
+	log.SetLogger(zap.New())
+}
+
+func main() {
+	entryLog := log.Log.WithName("entrypoint")
+
+	// Setup a Manager
+	entryLog.Info("setting up manager")
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		entryLog.Error(err, "unable to set up overall controller manager")
+		os.Exit(1)
+	}
+
+	// Setup a new controller to reconcile ReplicaSets
+	entryLog.Info("Setting up controller")
+	c, err := controller.New("foo-controller", mgr, controller.Options{
+		Reconciler: &reconcileReplicaSet{client: mgr.GetClient()},
+	})
+	if err != nil {
+		entryLog.Error(err, "unable to set up individual controller")
+		os.Exit(1)
+	}
+
+	if err := c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		entryLog.Error(err, "unable to watch pods")
+		os.Exit(1)
+	}
+
+	entryLog.Info("starting manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		entryLog.Error(err, "unable to run manager")
+		os.Exit(1)
+	}
+}
